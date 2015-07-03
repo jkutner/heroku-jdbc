ba0e521991ed920219b86f671ddae1f8458a3d8a
Joe Kutner <jpkutner@gmail.com>
2015-07-03 10:07:06 -0500

    First commit

---
+++ src/main/java/com/heroku/sdk/jdbc/DatabaseUrl.java
@@ -0,0 +1,78 @@
+package com.heroku.sdk.jdbc;
+
+import java.sql.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class DatabaseUrl {
+
+  private static String DEFAULT_SUB_PROTOCOL = "postgresql://";
+
+  private String dbStr;
+
+  private URI dbUri;
+
+  public static DatabaseUrl extract() throws URISyntaxException {
+    return extract("DATABASE_URL");
+  }
+
+  public static DatabaseUrl extract(String envVar) throws URISyntaxException {
+    return new DatabaseUrl(envVar);
+  }
+
+  public DatabaseUrl(String envVar) throws URISyntaxException {
+    this.dbStr = System.getenv(envVar);
+    if (this.dbStr == null || this.dbStr.isEmpty()) {
+      throw new IllegalArgumentException("Could not find value for " + envVar);
+    }
+    this.dbUri = new URI(this.dbStr);
+  }
+
+  public String username() {
+    if (dbUri.getUserInfo() != null) {
+      return dbUri.getUserInfo().split(":")[0];
+    } else {
+      return null;
+    }
+  }
+
+  public String password() {
+    if (dbUri.getUserInfo() != null) {
+      return dbUri.getUserInfo().split(":")[1];
+    } else {
+      return null;
+    }
+  }
+
+  public String host() {
+    return dbUri.getHost();
+  }
+
+  public Integer port() {
+    return dbUri.getPort();
+  }
+
+  public String path() {
+    return dbUri.getPath();
+  }
+
+  public String jdbcUrl() {
+    return jdbcUrl(DEFAULT_SUB_PROTOCOL);
+  }
+
+  public String jdbcUrl(String subProtocol) {
+    return "jdbc:" + subProtocol + host() + ':' + port() + path();
+  }
+
+  public Connection getConnection() throws SQLException {
+    return getConnection(DEFAULT_SUB_PROTOCOL);
+  }
+
+  public Connection getConnection(String subProtocol) throws SQLException {
+    if (username() != null) {
+      return DriverManager.getConnection(jdbcUrl(), username(), password());
+    } else {
+      return DriverManager.getConnection(jdbcUrl());
+    }
+  }
+}
